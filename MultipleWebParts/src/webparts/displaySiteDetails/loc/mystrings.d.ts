6fac1fdb609ee912e006d9a9e12fe1beb8f2b614
Harikrishna Gupta <harikrishnag@vconstruct.in>
2026-03-23 15:46:03 +0530

    Displaying user site details

---
+++ MultipleWebParts/src/webparts/displaySiteDetails/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IDisplaySiteDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'DisplaySiteDetailsWebPartStrings' {
+  const strings: IDisplaySiteDetailsWebPartStrings;
+  export = strings;
+}
